6402d7d019e0e2abcaf620e228cc691d1522f599
shinji.misaka <shinmisa0114@gmail.com>
2019-07-20 00:20:56 +0900

    Initial Commit

---
+++ iconChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iconChat
+//
+//  Created by 三坂真治 on 2019/07/20.
+//  Copyright © 2019 shinji.misaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
