7fc1b40d544fb47d7f93f238ee34b3cbba38f1c1
shinji.misaka <shinmisa0114@gmail.com>
2019-07-20 01:11:19 +0900

    アイコン画像の追加と、(アイコン選択/名前入力画面)のUIを作った。

--- iconChat/ViewController.swift
+++ iconChat/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //UIButtonを画像にする
+        button1.setImage(UIImage.init(named: "death"), for: UIControl.State.normal)
+        button2.setImage(UIImage.init(named: "girl"), for: UIControl.State.normal)
+        button3.setImage(UIImage.init(named: "god"), for: UIControl.State.normal)
+        button4.setImage(UIImage.init(named: "hime"), for: UIControl.State.normal)
+        button5.setImage(UIImage.init(named: "magicman"), for: UIControl.State.normal)
+        button6.setImage(UIImage.init(named: "man"), for: UIControl.State.normal)
+        button7.setImage(UIImage.init(named: "oldman"), for: UIControl.State.normal)
+        button8.setImage(UIImage.init(named: "sportman"), for: UIControl.State.normal)
+        button9.setImage(UIImage.init(named: "tono"), for: UIControl.State.normal)
+        
     }
-
+    
+    @IBAction func entryButton(_ sender: Any) {
+        
+    }
+    
 
 }
 
