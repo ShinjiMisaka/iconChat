cee0cbbcd9ad08dd7877ccfc3077db7d7bf785cb
shinji.misaka <shinmisa0114@gmail.com>
2019-07-26 11:13:16 +0900

    部屋一覧画面と部屋作成画面を追加した。

---
+++ iconChat/roomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  roomTableViewCell.swift
+//  iconChat
+//
+//  Created by 三坂真治 on 2019/07/26.
+//  Copyright © 2019 shinji.misaka. All rights reserved.
+//
+
+import UIKit
+
+class roomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
